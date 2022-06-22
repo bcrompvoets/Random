Corrected units on c2d surveys

Phase_3_1__IRAC_Only_Classification/Notebooks/network_runner.py
@@ -21,11 +21,15 @@ device = torch.device("cpu")
 print(f'Running on : {device}')
 
 # YSO_EG_Stars Train
-X_tr = np.load("../Data_and_Results/c2d_Inputs_CLOUDS_Train.npy") # Load input data
-Y_tr = np.load("../Data_and_Results/c2d_Targets_CLOUDS_Train.npy") # Load target data
+# X_tr = np.load("../Data_and_Results/c2d_Inputs_CLOUDS_Train.npy") # Load input data
+# Y_tr = np.load("../Data_and_Results/c2d_Targets_CLOUDS_Train.npy") # Load target data
+# inp_tr, tar_tr = replicate_data_single(X_tr, Y_tr, [1082,1082,1082])
+
+X_tr = np.load("../Data_and_Results/Inputs_YSO_EG_Stars_alpha.npy") # Load input data
+Y_tr = np.load("../Data_and_Results/Targets_YSO_EG_Stars_alpha.npy") # Load target data
 X_tr = np.float32(X_tr)
 Y_tr = np.float32(Y_tr)
-inp_tr, tar_tr = replicate_data_single(X_tr, Y_tr, [1082,1082,1082])
+inp_tr, tar_tr = replicate_data_single(X_tr, Y_tr, [1500,1500,1500])
 
 # YSO_EG_Stars Valid
 X_va = np.load("../Data_and_Results/c2d_Inputs_CORES_Valid.npy") # Load input data
@@ -35,11 +39,11 @@ Y_va = np.float32(Y_va)
 inp_va, tar_va = replicate_data_single(X_va, Y_va, [314,212,4269])
 
 # YSO_EG_Stars Test
-X_te = np.load("../Data_and_Results/Rap_Inputs_Test.npy") # Load input data
-Y_te = np.load("../Data_and_Results/Rap_Targets_Test.npy") # Load target data
-X_te = np.float32(X_te)
-Y_te = np.float32(Y_te)
-inp_te, tar_te = replicate_data_single(X_te, Y_te, [553,373,7518])
+inp_te = np.load("../Data_and_Results/c2d_Inputs_CLOUDS_Train.npy")#Rap_Inputs_Test.npy") # Load input data
+tar_te = np.load("../Data_and_Results/c2d_Targets_CLOUDS_Train.npy")#Rap_Targets_Test.npy") # Load target data
+inp_te = np.float32(inp_te)
+tar_te = np.float32(tar_te)
+# inp_te, tar_te = replicate_data_single(X_te, Y_te, [553,373,7518])
 
 # YSO 
 # X = np.load("../Data_and_Results/Inputs_YSO_Train.npy") # Load input data
@@ -60,7 +64,7 @@ if __name__ == '__main__':
     momentum_vals = [0.6, 0.75, 0.9]
     learning_rate_vals = [1e-1, 1e-2, 1e-3]
     epochs = 3000
-    filepath = "../MLP_Runs_Results/YSO_EG_Stars/c2dTrain/"
+    filepath = "../MLP_Runs_Results/YSO_EG_Stars/c2dValid/"
     filepaths = [filepath+"OneLayer_", filepath+"TwoLayer_", filepath+"FiveLayer_"]
 
     # We want to run a loop over the momentum and learning rate values, and use the